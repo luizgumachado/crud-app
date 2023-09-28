e2f4e8531be85075dc045f49568f54daad87b354
Luiz Gustavo Souza <luizgustavo.gumachado@gmail.com>
2023-09-28 00:27:51 -0300

    added delete function to cards

--- scripts/lista.js
+++ scripts/lista.js
@@ -1,6 +1,6 @@
 const arrayJogos = JSON.parse(localStorage.getItem("arrayJogos")) || [];
 
-arrayJogos.forEach((jogo) => {
+arrayJogos.forEach((jogo, index) => {
     const cardJogo = document.createElement("div");
     cardJogo.className = "card-jogo";
 
@@ -25,12 +25,19 @@ arrayJogos.forEach((jogo) => {
 
     const removeBtn = document.createElement("div");
     removeBtn.className = "remove-btn";
-    removeBtn.onclick = "removeGame()";
+    removeBtn.onclick = `removeGame(${jogo.id})`;
     removeBtn.innerHTML = `
         <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25" viewBox="0 0 32 32">
             <path d="M 7.21875 5.78125 L 5.78125 7.21875 L 14.5625 16 L 5.78125 24.78125 L 7.21875 26.21875 L 16 17.4375 L 24.78125 26.21875 L 26.21875 24.78125 L 17.4375 16 L 26.21875 7.21875 L 24.78125 5.78125 L 16 14.5625 Z"></path>
         </svg>
     `;
+    removeBtn.addEventListener("click", () => {
+        if (confirm("Deseja remover o jogo da lista?")) {
+            arrayJogos.splice(index, 1);
+            localStorage.setItem("arrayJogos", JSON.stringify(arrayJogos));
+            location.reload();
+          }
+    });
 
     cardJogo.appendChild(imagemJogo);
     cardJogo.appendChild(tituloJogo);
